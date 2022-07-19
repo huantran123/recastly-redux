c5ccae28604de7b49489443b335fa850a62767cd
Huan Tran <huanductran98@gmail.com>
2022-07-19 01:12:30 -0700

    Complete connecting combine reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
-// const store = Redux.createStore(rootReducer, Redux.applyMiddleware(thunk));
+// const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunk)
+// const composedEnhancers = compose(middlewareEnhancer, monitorReducerEnhancer)
+const store = createStore(rootReducer, {videoList: [], currentVideo: {}}, applyMiddleware(thunk));
+
+export default store;
